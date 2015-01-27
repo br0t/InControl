dd5286a24b943e379fa519caa42f15845936219a
Thomas Keller <tom@hellforge.co>
2015-01-27 23:25:42 +0100

    updated VersionUpdater path

--- Assets/Editor/VersionUpdater.cs
+++ Assets/Editor/VersionUpdater.cs
@@ -28,7 +28,7 @@ namespace InControl
 
 		static void UpdateVersion()
 		{
-			string versionPath = "Assets/InControl/Source/VersionInfo.cs";
+			string versionPath = "Assets/InControl/Assets/InControl/Source/VersionInfo.cs";
 			string versionText = GetFileContents( versionPath );
 			if (versionText != null)
 			{
